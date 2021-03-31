3070a66c22e3339b2cf66c1fb03f0c866dd81b35
Admin <Admin@ya.com>
2021-03-31 07:47:18 +0300

    new

--- .eslintrc.js
+++ .eslintrc.js
@@ -5,12 +5,13 @@ module.exports = {
         'plugin:@typescript-eslint/recommended',
     ],
     parserOptions: {
-        ecmaVersion: 2020,  // Allows for the parsing of modern ECMAScript features
+        ecmaVersion: 2018,  // Allows for the parsing of modern ECMAScript features
         sourceType: 'module',  // Allows for the use of imports
         ecmaFeatures: {
             jsx: true,  // Allows for the parsing of JSX
         },
     },
+
     rules: {
         // Place to specify ESLint rules. Can be used to overwrite rules specified from the extended configs
         // e.g. "@typescript-eslint/explicit-function-return-type": "off",
@@ -18,6 +19,9 @@ module.exports = {
         "react/jsx-filename-extension": "off",
         "jsx-a11y/anchor-is-valid": "off",
         "jsx-a11y/img-redundant-alt": "off",
+        "react/prop-types": "off",
+        "@typescript-eslint/no-empty-function": "off"
+
     },
     settings: {
         react: {
