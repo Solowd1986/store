5acb0fe3a5916b8193bb0d1b3951c85ac1e0859a
Admin <Admin@ya.com>
2021-03-31 08:41:50 +0300

    new

--- config-overrides.js
+++ config-overrides.js
@@ -6,7 +6,8 @@ module.exports = function override(config, env) {
     '@root': path.resolve('src/'),
     '@components': path.resolve('src/components/'),
     '@redux': path.resolve('src/redux/'),
-    '@scss': path.resolve('src/assets/scss/'),
+    //'@scss': path.resolve('src/assets/scss/'),
+    '@scss': path.resolve(__dirname, 'src/assets/scss/')
   })(config, env);
 
   return config;
